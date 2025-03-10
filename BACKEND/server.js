9bf78922b4980ab2bbc99f18997de41ac1c24cd7
anushka3285 <anushkakakade8@gmail.com>
2025-03-10 15:01:40 +0530

    adding all files

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,4 +1,4 @@
-const express=require('express')
+/*const express=require('express')
 const mongoose=require('mongoose')
 require('dotenv').config()
 const User=require('./models/User')
@@ -64,4 +64,4 @@ app.listen(PORT,(err)=>{
         console.log(err)
     }
     console.log("Server is running on port :"+PORT)
-})
+})*/
